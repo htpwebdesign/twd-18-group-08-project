48dd699f2b10b944740ecd7e293a039cfe55f86e
Hanna <hannac222@gmail.com>
2018-03-04 23:31:34 -0800

    Add a hambuger menu

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,9 @@
 // Insert JavaScript here...
+const body = document.body;
+const btn = document.querySelector('.menu-mobile');
+
+btn.addEventListener('click', function(){
+
+    body.classList.toggle('show');
+}
+)
